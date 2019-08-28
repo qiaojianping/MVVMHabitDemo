49f7c23de6b11aba0f145d196b3a563c3a0890b1
Deft <253386581@qq.com>
2019-08-28 14:58:17 +0800

    rxjava base use

--- myrxjava/src/main/java/com/xgimi/myrxjava/MainActivity.java
+++ myrxjava/src/main/java/com/xgimi/myrxjava/MainActivity.java
@@ -2,12 +2,41 @@ package com.xgimi.myrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("1111");
+                sleep(1000);
+                emitter.onNext("2222");
+                emitter.onNext("3333");
+                emitter.onComplete();
+            }
+        }).observeOn(AndroidSchedulers.mainThread())//回调给主线程
+                .subscribeOn(Schedulers.io())//执行在io线程
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG,"S:" + s);
+                    }
+                });
     }
 }
